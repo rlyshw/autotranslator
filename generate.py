e193cdf3b2282927d8e06371048964fc73db10c4
Riley Shaw <rlyshw@gmail.com>
2014-03-12 18:55:53 -0400

    Cleaning
    Tidied things up, adhering (loosely) to PEP-8

--- generate.py
+++ generate.py
@@ -1,24 +1,28 @@
 import sys, argparse, csv, goslate
 
-def translate(word, lang, api):
+def translate(word, target, detect, source, api):
     if api == "goog":
         gs = goslate.Goslate()
-        return gs.translate(str(word), str(lang))
-    
+        if detect:
+            return gs.translate(str(word), str(target), gs.detect(source))
+        elif not detect:
+            return gs.translate(str(word), str(target), source)
+
 def main():
     inputfile = 'in.csv'
     outputfile = 'out.csv'
-    langs = ['zh','de']
+    langs = ['zh','de','fr']
     api = 'goog'
     try:
         with open(inputfile, 'rb') as infile:
-            inreader = csv.reader(infile)
+            inputreader = csv.reader(infile)
             with open(outputfile, 'wb+') as outfile:
                 outwords = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL,
                                       delimiter=',')
-                for row in inreader:
+                for row in inputreader:
                     for lang in langs:
-                        row.append(translate(row[0],lang, api).encode('utf-8'))
+                        row.append(translate(row[0], lang, True,
+                                   row[0], api).encode('utf-8'))
                     outwords.writerow(row)
     except IOError as e:
         print 'cannot open', inputfile
