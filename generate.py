6572e5abe12cf85aaed352f76784d0d5f2e9e823
Riley Shaw <rlyshw@gmail.com>
2014-03-13 16:25:16 -0400

    Changed Lang selection
    Language selection is now done directly inside of the in.csv file

--- generate.py
+++ generate.py
@@ -1,31 +1,24 @@
 import sys, argparse, csv, goslate
 
-def translate(word, target, detect, source, api):
+def translate(word, target, source, api):
     gs = goslate.Goslate()
     if api == "goog":
-        if detect:
-            return gs.translate(str(word), str(target), gs.detect(source))
-        elif not detect:
-            return gs.translate(str(word), str(target), source)
+        return gs.translate(str(word), str(target), source)
 
 def main():
     inputfile = 'in.csv'
     outputfile = 'out.csv'
-    langs = ['zh','de','fr']
     api = 'goog'
-    try:
-        with open(inputfile, 'rb') as infile:
-            inputreader = csv.reader(infile)
-            with open(outputfile, 'wb+') as outfile:
-                outwords = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
-                for row in inputreader:
-                    for lang in langs:
-                        row.append(translate(row[0], lang, True,
-                                   row[0], api).encode('utf-8'))
-                    outwords.writerow(row)
-    except IOError as e:
-        print 'cannot open', inputfile
-        print e.strerror
+    with open(inputfile, 'rb+') as infile:
+        inputreader = csv.reader(infile)
+        langs = inputreader.next()
+        with open(outputfile, 'wb+') as outfile:
+            outwords = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
+            for row in inputreader:
+                for l in range(1,len(langs)):
+                    row.append(translate(row[0], langs[l],
+                               langs[0], api).encode('utf-8'))
+                outwords.writerow(row)
 
 if __name__ == "__main__":
    main()
